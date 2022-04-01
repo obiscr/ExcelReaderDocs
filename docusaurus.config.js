fa059534f83341fabec1aa828c81ffb3e4547c14
piercebrands <piercebrands@gmail.com>
2022-04-02 00:36:23 +0800

    Fix editUrl error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,13 +39,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/obiscr/ExcelReaderDocs/tree/main/docs/',
+          editUrl: 'https://github.com/obiscr/ExcelReaderDocs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/obiscr/ExcelReaderDocs/tree/main/blog/',
+            'https://github.com/obiscr/ExcelReaderDocs/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
